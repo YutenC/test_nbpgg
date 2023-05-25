ca9148ca77e870c91036b19a4a11dcaf1651af1d
YutenC <max875e6@gmail.com>
2023-05-25 14:08:38 +0800

    project initialize

---
+++ src/main/java/Teset_Controller.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Test_Controller")
+public class Teset_Controller extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter writer = resp.getWriter();
+		writer.print("Hello");
+				
+	}
+	
+
+}
